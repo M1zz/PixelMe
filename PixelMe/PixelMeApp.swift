a0c772c15e85b228daaa0438c1f0a0c2451f0d28
Leeo <mizzking75@gmail.com>
2023-06-01 08:49:18 +0900

    Initial Commit

---
+++ PixelMe/PixelMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelMeApp.swift
+//  PixelMe
+//
+//  Created by hyunho lee on 2023/06/01.
+//
+
+import SwiftUI
+
+@main
+struct PixelMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
